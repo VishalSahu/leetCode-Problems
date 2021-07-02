e1a6a081dad6179b72a2f170031cc8b447a4dbca
VishalSahu <52187641+VishalSahu@users.noreply.github.com>
2021-07-02 11:14:09 +0530

    Create Median_of_two_sorted_arrays.java

---
+++ Median_of_two_sorted_arrays.java
@@ -0,0 +1,66 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2)
+    {
+        int m = nums1.length;
+        int n = nums2.length;
+        int len = m+n;
+        int[] merg = new int[n+m];
+        int i,j;
+        double res = 0.0d;
+        System.arraycopy(nums1,0,merg,0,m);
+        System.arraycopy(nums2,0,merg,m,n);
+        Arrays.sort(merg);
+        if((len)%2!=0)
+        {
+            res = merg[len/2];
+            
+        }
+        else
+        {
+            int midterm = merg[len/2] + merg[(len-1)/2];
+            res = ((double)midterm)/2;
+        }
+        return res;
+    }
+}
+
+/*
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+Example 3:
+
+Input: nums1 = [0,0], nums2 = [0,0]
+Output: 0.00000
+Example 4:
+
+Input: nums1 = [], nums2 = [1]
+Output: 1.00000
+Example 5:
+
+Input: nums1 = [2], nums2 = []
+Output: 2.00000
+ 
+
+Constraints:
+
+nums1.length == m
+nums2.length == n
+0 <= m <= 1000
+0 <= n <= 1000
+1 <= m + n <= 2000
+-106 <= nums1[i], nums2[i] <= 106
+*/
